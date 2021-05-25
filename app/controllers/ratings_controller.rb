13890cf6c48af5119874a9869d3d4e904de6c901
Breanna Dodd <bdodd@salesforce.com>
2021-05-25 13:12:04 -0600

    commit all sadness

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,86 @@
+class RatingsController < ApplicationController
+  before_action :set_rating, only: %i[ show edit update destroy ]
+
+  # GET /ratings or /ratings.json
+  def index
+    @ratings = Rating.all
+  end
+
+  private def get_zip_from_location
+    return "90210"
+  end
+
+  require OpenURI
+
+
+  private def fun_type
+    URI.open("http://www.ruby-lang.org/") {|f|
+      f.each_line {|line| p line}
+    }
+  end
+
+  # GET /ratings/1 or /ratings/1.json
+  def show
+    @rating = OpenStruct.new({
+      fun_type: 2,
+      suggested_direction: "south",
+      water_requirement: 3
+      })
+  end
+
+  # GET /ratings/new
+  def new
+    @rating = Rating.new
+  end
+
+  # GET /ratings/1/edit
+  def edit
+  end
+
+  # POST /ratings or /ratings.json
+  def create
+    @rating = Rating.new(rating_params)
+
+    respond_to do |format|
+      if @rating.save
+        format.html { redirect_to @rating, notice: "Rating was successfully created." }
+        format.json { render :show, status: :created, location: @rating }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @rating.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /ratings/1 or /ratings/1.json
+  def update
+    respond_to do |format|
+      if @rating.update(rating_params)
+        format.html { redirect_to @rating, notice: "Rating was successfully updated." }
+        format.json { render :show, status: :ok, location: @rating }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @rating.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /ratings/1 or /ratings/1.json
+  def destroy
+    @rating.destroy
+    respond_to do |format|
+      format.html { redirect_to ratings_url, notice: "Rating was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_rating
+    end
+
+    # Only allow a list of trusted parameters through.
+    def rating_params
+      params.require(:rating).permit(:fun_type, :suggested_direction, :water_requirement)
+    end
+end
---
+++ app/views/ratings/_rating.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! rating, :id, :fun_type, :suggested_direction, :water_requirement, :created_at, :updated_at
+json.url rating_url(rating, format: :json)
